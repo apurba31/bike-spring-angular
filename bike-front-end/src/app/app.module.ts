7283c059af3489a53cff3d07b2bb2d685f67c1b7
Apurba Kumar <apurbakumar.3110@gmail.com>
2021-04-10 13:35:29 +0530

    Code for bike front end in progress

---
+++ bike-front-end/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BikeService } from './services/bike.service';
+import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [BikeService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
